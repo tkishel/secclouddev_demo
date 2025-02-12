63f01579fc8902e51e41d4244872dad766901434
Tom Kishel <tkishel@gmail.com>
2025-02-12 07:50:32 -0800

    Test 3

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 resource "aws_s3_bucket" "example_bucket" {
   bucket            = "my-example_bucket"
-  block_public_acls = true
+}
+
+resource "aws_s3_bucket_public_access_block" "disable_block_public_access" {
+  bucket                  = aws_s3_bucket.example_bucket.id
+  block_public_acls       = false
 }
